4137dc3c3ba6e8cc5968f0fac4bfeeeaa7f1441b
Yogiswar-21 <kotayogi2004@gmail.com>
2025-07-22 23:28:04 +0530

    fix

--- src/components/GoogleAuthButton.jsx
+++ src/components/GoogleAuthButton.jsx
@@ -4,7 +4,7 @@ import { FcGoogle } from 'react-icons/fc';
 
 const GoogleAuthButton = () => (
   <a
-    href="http://localhost:8080/auth/google"
+    href="https://sports-backend-wxdt.onrender.com/auth/google"
     className="flex items-center justify-center gap-2 w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm hover:bg-gray-100 transition text-black"
   >
     <FcGoogle size={20} />
